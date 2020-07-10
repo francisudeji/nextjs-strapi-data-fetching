cd801015edf5ade8cc246ae027871e68892eace3
francisudeji <francis.udejiofficial@gmail.com>
2020-07-10 04:31:51 +0100

    Export base url from local env and a fetcher function

---
+++ frontend/utils.js
@@ -0,0 +1,18 @@
+const baseUrl = process.env.BASE_URL
+
+async function fetchQuery(path, params = null) {
+	let url
+
+	if (params !== null) {
+		url = `${baseUrl}/${path}/${params}`
+	} else {
+		url = `${baseUrl}/${path}`
+	}
+
+	const response = await fetch(`${url}`)
+	const data = await response.json()
+
+	return data
+}
+
+export { baseUrl, fetchQuery }
